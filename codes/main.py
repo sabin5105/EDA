c7e26af4be20b216a6fedf1cd242d91cafb16267
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-05-24 21:27:40 +0900

    clear

---
+++ codes/main.py
@@ -0,0 +1,7 @@
+# if __name__ == '__main__':
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import sys
+
+df = pd.read_csv('/Users/sabin/Documents/GitHub/EDA/data/서울시_기간별_일평균_대기환경_정보.csv', encoding='CP949')
